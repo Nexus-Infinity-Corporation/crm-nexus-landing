6116baf71ea0209421daf368667240e723d64742
Raikonif <raikonif@gmail.com>
2026-03-03 18:53:21 -0400

    fix: switch adapter from node to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
 import { fileURLToPath } from "node:url";
 
 export default defineConfig({
-  adapter: node({ mode: "standalone" }),
+  adapter: vercel(),
   integrations: [tailwind(), react()],
   vite: {
     resolve: {
