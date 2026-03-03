b551be17e0b90e4bc15c80f1f878e64d081a9198
Diego Alarcón Inturias <raikonif@gmail.com>
2026-03-03 18:39:00 -0400

    feat: implemented demo-form (#10)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import node from "@astrojs/node";
 import { fileURLToPath } from "node:url";
 
 export default defineConfig({
+  adapter: node({ mode: "standalone" }),
   integrations: [tailwind(), react()],
   vite: {
     resolve: {
